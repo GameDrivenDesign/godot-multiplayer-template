8041582da3d945828c129f85020c79f44e68bd0d
Tom Beckmann <tomjonabc@gmail.com>
2021-12-20 13:26:10 +0100

    add windows support to build script

--- components/build-client.sh
+++ components/build-client.sh
@@ -4,13 +4,25 @@
 
 set -e
 
-# NAME=$1
+OS=$1
+
+case "$OS" in
+    "win")
+    ENGINE_DEFAULT="../Godot_v3.4.1-stable_win64.exe"
+    TEMPLATE="Windows Desktop"
+    ;;
+    "linux")
+    ENGINE_DEFAULT="../Godot_v3.4-stable_linux_headless.64"
+    TEMPLATE="Linux/X11"
+    ;;
+esac
+
 # TODO: jump to right platform and get the specific export_path
 NAME=$(grep 'export_path' export_presets.cfg | cut -c14- | sed 's/.$//')
-ENGINE=${1:-../Godot_v3.4-stable_linux_headless.64}
+ENGINE=${2:-$ENGINE_DEFAULT}
 
 ORIGINAL_DATE=$(date -r project.godot "+%Y-%m-%d %H:%M:%S")
-$ENGINE --export-debug "Linux/X11" || exit 1
+$ENGINE --no-window --export-debug "$TEMPLATE" || exit 1
 touch -m --date="$ORIGINAL_DATE" project.godot
 
 $NAME --client
